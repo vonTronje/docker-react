2c93dc4be6ca9d6e919d971f205629eabd68dd57
Hagen Mahnke <hagen.mahnke@marleyspoon.com>
2021-02-12 17:10:33 +0100

    Change for demonstrating feature branch work flow

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          ABHJASBJFK
+          FEATURE BRANCH
         </p>
         <a
           className="App-link"
